bc651b165e974b15bc2b997a0f5e599a6f4be924
anass-ghammadi <anassghammadi@gmail.com>
2021-12-30 23:27:23 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import Menu from'./Menu'
 import Slide from './slide'
-import Main1 from './main1'
-
+import Main1 from './main-1'
+import Main2 from './main-2'
+import Main3 from './main-3'
+import Main4 from './main-4'
+import Main5 from './main-5'
 function App() {
   return (
     <>
@@ -11,6 +14,10 @@ function App() {
      
       <Slide />
       <Main1 />
+      <Main2 />
+      <Main3 />
+      <Main4 />
+      <Main5 />
     </>
   );
 }
