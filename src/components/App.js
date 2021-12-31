55dc8e471d963f885044d7e028bbde156fb8611d
anass-ghammadi <anassghammadi@gmail.com>
2022-01-01 00:49:05 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,10 @@ import Main2 from './main-2'
 import Main3 from './main-3'
 import Main4 from './main-4'
 import Main5 from './main-5'
+import Footer1 from '../components/footer/footer-1'
+import Footer2 from '../components/footer/footer-2'
+import Footer3 from '../components/footer/footer-3'
+import Footer4 from '../components/footer/footer-4'
 function App() {
   return (
     <>
@@ -18,6 +22,10 @@ function App() {
       <Main3 />
       <Main4 />
       <Main5 />
+      <Footer1 />
+      <Footer2 />
+      <Footer3 />
+      <Footer4 />
     </>
   );
 }
