9de58dcfdb3ec0cf9c9cd562310410d681da81aa
anass-ghammadi <anassghammadi@gmail.com>
2022-01-11 15:01:41 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,31 +1,32 @@
+import { AboutSVG, AccountSVG, ContactSVG, HomeSVG, SearchSVG, ServicesSVG } from '../assets/Svgs';
 import '../styles/menu.css';
-function Menu() 
-{ 
+function Menu() {
     return (
-        
-        <header className="header">
+
+        <div className="menu-container">
+            <header className="header">
                 <nav className="nav">
-                   <div className="nav-logo">
+                    <div className="nav-logo">
                         <a href="#">LOGO</a>
-                    </div> 
+                    </div>
                     <div className="content">
                         <div className="nav-menu">
                             <ul className="menu">
-                                <li> 
-                                    <a href="#"><div className="icone"><Home /></div>Accueil</a> 
-                                   
+                                <li>
+                                    <a href="#"><div className="icone"><HomeSVG /></div>Accueil</a>
+
                                 </li>
                                 <li>
-                                    <a href="#"><div className="icone"><Services /></div> Services</a> 
-                                   
+                                    <a href="#"><div className="icone"><ServicesSVG /></div> Services</a>
+
                                 </li>
                                 <li>
-                                    <a href="#"><div className="icone"><About /></div>About</a>
-                                    
+                                    <a href="#"><div className="icone"><AboutSVG /></div>About</a>
+
                                 </li>
                                 <li>
-                                    <a href="#"><div className="icone"><Contact /></div>Contact</a>
-                                    
+                                    <a href="#"><div className="icone"><ContactSVG /></div>Contact</a>
+
                                 </li>
                             </ul>
                         </div>
@@ -33,88 +34,23 @@ function Menu()
                             <ul className="nav-icon">
                                 <div>
                                     <li>
-                                        <Search />
-                                     </li>
+                                        <SearchSVG />
+                                    </li>
                                 </div>
                                 <div>
-                                     <li>
-                                        <Account />
+                                    <li>
+                                        <AccountSVG />
                                     </li>
                                 </div>
                             </ul>
                         </div>
                     </div>
                 </nav>
-        </header>
-        
+            </header>
+        </div>
     )
 
 }
 
-export const Account = () => {
 
-    return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M22 8H2V22H22V8Z" fill="currentColor" fill-opacity="0.3"/>
-            <path d="M12 16.5C8.96 16.5 6.5 18.96 6.5 22C6.51 22 17.5 22 17.5 22C17.5 18.96 15.04 16.5 12 16.5Z" fill="currentColor" fill-opacity="0.6"/>
-            <path d="M12 16.5C13.5188 16.5 14.75 15.2688 14.75 13.75C14.75 12.2312 13.5188 11 12 11C10.4812 11 9.25 12.2312 9.25 13.75C9.25 15.2688 10.4812 16.5 12 16.5Z" fill="currentColor" fill-opacity="0.6"/>
-            <path d="M8 2H16" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M5 5H19" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    )
-}
-
-export const Search = () => {
-
-    return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M16.0001 15.2878L15.293 15.9949L21.2962 21.9982L22.0033 21.2911L16.0001 15.2878Z" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18Z" fill="currentColor" fill-opacity="0.3" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-
-    )
-}
-
-export const Home = () => {
-
-    return (
-        <svg width="25" height="22" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-         <path d="M20.3398 9.86304H4.33984V21.6987H20.3398V9.86304Z" fill="currentColor" fill-opacity="0.3"/>
-         <path d="M14.3398 13.8082H10.3398V21.6986H14.3398V13.8082Z" fill="currentColor" fill-opacity="0.6"/>
-         <path d="M12.3398 1.9726L2.33984 9.86301H22.3398L12.3398 1.9726Z" fill="currentColor" fill-opacity="0.3"/>
-        </svg>
-
-    )
-}
-
-export const Services = () => {
-
-    return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.8696 4.89L13.5596 2.44C13.5296 2.19 13.3196 2 13.0596 2H9.94962C9.69962 2 9.47962 2.19 9.44962 2.44L9.17962 4.59C9.15962 4.77 9.02962 4.93 8.85962 4.99C8.01962 5.3 7.21962 5.78 6.52962 6.4L4.24962 5.44C4.01962 5.34 3.74962 5.43 3.61962 5.65L2.05962 8.35C1.93962 8.57 1.99962 8.85 2.19962 9L4.16962 10.49C3.95962 11.5 3.95962 12.53 4.15962 13.51H4.16962L2.19962 15C1.99962 15.15 1.93962 15.43 2.06962 15.65L3.62962 18.35C3.75962 18.57 4.02962 18.66 4.25962 18.56L6.53962 17.6L6.52962 17.61C6.89962 17.94 7.31962 18.24 7.76962 18.5C8.21962 18.76 8.67962 18.97 9.15962 19.13V19.11L9.46962 21.56C9.47962 21.81 9.69962 22 9.94962 22H13.0696C13.3196 22 13.5296 21.81 13.5696 21.56L13.8396 19.41C13.8596 19.23 13.9896 19.07 14.1596 19.01C14.9996 18.7 15.7996 18.22 16.4896 17.6L18.7696 18.56C18.9996 18.66 19.2696 18.57 19.3996 18.35L20.9596 15.65C21.0896 15.43 21.0296 15.15 20.8296 15L18.8596 13.51C19.0696 12.5 19.0696 11.47 18.8696 10.49H18.8596L20.8096 9C21.0096 8.85 21.0696 8.57 20.9396 8.35L19.3796 5.65C19.2496 5.43 18.9796 5.34 18.7496 5.44L16.4796 6.4L16.4896 6.39C16.1196 6.06 15.6996 5.76 15.2496 5.5C14.7996 5.24 14.3396 5.03 13.8596 4.87" fill="currentColor" fill-opacity="0.3"/>
-            <path d="M11.5098 16C13.7189 16 15.5098 14.2091 15.5098 12C15.5098 9.79086 13.7189 8 11.5098 8C9.30063 8 7.50977 9.79086 7.50977 12C7.50977 14.2091 9.30063 16 11.5098 16Z" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    )
-}
-
-export const About = () => {
-
-    return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11.0002 2C8.66016 2 5.71016 3.72 5.76016 8.16L2.04016 12.98C1.93016 13.12 2.06016 13.3 2.26016 13.3L4.74016 14.53C4.87016 15.45 5.35016 18.13 5.61016 19.47C5.70016 19.89 9.80016 19.57 9.80016 19.57L11.0002 22" fill="currentColor" fill-opacity="0.3"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M15.8447 2.20224C15.9563 1.89025 15.7939 1.54682 15.4819 1.43516C15.17 1.3235 14.8265 1.4859 14.7149 1.79789L13.6949 4.64789C13.5832 4.95988 13.7456 5.30332 14.0576 5.41498C14.3696 5.52664 14.713 5.36424 14.8247 5.05224L15.8447 2.20224ZM22.0491 5.22039C22.3144 5.02182 22.3684 4.64579 22.1699 4.38051C21.9713 4.11522 21.5953 4.06114 21.33 4.25972L17.91 6.81972C17.6447 7.01828 17.5906 7.39432 17.7892 7.65961C17.9878 7.92488 18.3638 7.97896 18.6291 7.78039L22.0491 5.22039ZM18.1311 11.8261C18.1554 11.4957 18.4429 11.2474 18.7734 11.2717L22.0434 11.5117C22.3739 11.5359 22.6221 11.8235 22.5979 12.154C22.5736 12.4845 22.2861 12.7327 21.9556 12.7085L18.6856 12.4685C18.3551 12.4442 18.1068 12.1566 18.1311 11.8261ZM11.7197 17.3101C11.3884 17.3101 11.1197 17.5787 11.1197 17.9101C11.1197 18.2415 11.3884 18.5101 11.7197 18.5101H14.3497C14.6811 18.5101 14.9497 18.2415 14.9497 17.9101C14.9497 17.5787 14.6811 17.3101 14.3497 17.3101H11.7197ZM11.1197 15.33C11.1197 14.9987 11.3884 14.73 11.7197 14.73H11.8791C10.5599 13.6227 8.75387 11.193 10.2298 9.25008C11.6698 7.36009 14.3998 7.36009 15.8398 9.25008C17.3142 11.191 15.5204 13.6245 14.1981 14.73H14.3497C14.6811 14.73 14.9497 14.9987 14.9497 15.33C14.9497 15.6614 14.6811 15.93 14.3497 15.93H11.7197C11.3884 15.93 11.1197 15.6614 11.1197 15.33ZM13.6398 14.73V12.1748C13.9183 11.9752 14.0998 11.6489 14.0998 11.2801C14.0998 10.6726 13.6073 10.1801 12.9998 10.1801C12.3923 10.1801 11.8998 10.6726 11.8998 11.2801C11.8998 11.6831 12.1165 12.0355 12.4398 12.2271V14.73H13.6398ZM12.9727 11.1838C12.9855 11.1824 12.9984 11.1814 13.0114 11.1808C13.0076 11.1803 13.0037 11.1801 12.9998 11.1801C12.9904 11.1801 12.9813 11.1814 12.9727 11.1838Z" fill="currentColor" fill-opacity="0.6"/>
-        </svg>
-    )
-}
-
-export const Contact = () => {
-
-    return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M18 20H6" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M22 4H2V16H22V4Z" fill="currentColor" fill-opacity="0.3" stroke="currentColor" stroke-opacity="0.6" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-        
-    )
-}
 export default Menu;
