06994e2070647d09057fc36ea0917364cca96744
anass-ghammadi <anassghammadi@gmail.com>
2022-01-11 22:59:09 +0100

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
     return (
         <>
-           <p>salut ici c'est contact</p>
+           <p className=" text-2xl text-center shadow-2xl shadow-cyan-500/50 bg-cyan-500">salut ici c'est contact</p>
             
         </>
     )
