70284940e02d7b6365a8a439b8f28dbe6fbeb593
anass-ghammadi <anassghammadi@gmail.com>
2022-01-11 18:56:17 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Menu from "./components/Menu";
+import Home from "./pages/home";
+
+
+function App() {
+    return (
+        <>
+            <Menu />
+            <Home />
+        </>
+    );
+}
+
+export default App;
