e416e8fa4ff63cefeee37d89b016a8fddd703e0b
anass-ghammadi <anassghammadi@gmail.com>
2022-01-11 20:54:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Menu from "./components/Menu";
 import Home from "./pages/home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
     return (
         <>
-            <Menu />
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    
+                    <Route path="/" index element={<Home />}/>
+                    <Route path="Services" index element={<Services />}/>
+                    <Route path="About" index element={<About />}/>
+                    <Route path="Contact" index element={<Contact />}/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
