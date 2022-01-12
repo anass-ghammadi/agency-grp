eebab371b984219c807ae28f53160d7ed48633fc
anass-ghammadi <anassghammadi@gmail.com>
2022-01-12 22:02:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import Menu from "./components/Menu";
+import React,{useState, useEffect} from 'react';
+import Menu from "./components/Menu/Menu";
+import MenuMobile from "./components/Menu/menu-mobile";
 import Home from "./pages/home";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import './styles/index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    useEffect(() => {
+        const HidesMenu = () => {
+            if(window.innerWidth > 768 && isOpen) {
+                setIsOpen(false)
+                console.log('I resized')
+            }
+        }
+        window.addEventListener('resize',HidesMenu)
+
+        return () => {
+            window.removeEventListener('resize',HidesMenu)
+        }
+    })
     return (
-        <>
+        <>  
+            
             <BrowserRouter>
+            <Menu toggle={toggle}/>
+            <MenuMobile isOpen={isOpen} toggle={toggle} />
                 <Routes>
                     
                     <Route path="/" index element={<Home />}/>
