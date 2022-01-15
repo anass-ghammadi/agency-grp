3848d2c99269c3f68dcdf35eb4dc1245d0c15e22
anass-ghammadi <anassghammadi@gmail.com>
2022-01-15 20:13:18 +0100

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,13 @@
 import HeroSection from "../components/molecule/HeroSection"
+import Section1 from "../components/Section-1"
+import Section2 from "../components/Section-2"
 
 const Home = () => {
     return (
         <>
             <HeroSection />
+            <Section1 />
+            <Section2 />
         </>
     )
 }
