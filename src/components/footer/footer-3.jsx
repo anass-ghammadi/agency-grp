ecd9ea6433e6bee368850b899d35d8942bc14839
anass-ghammadi <anassghammadi@gmail.com>
2022-01-18 00:35:25 +0100

    Update footer-3.jsx

--- src/components/footer/footer-3.jsx
+++ src/components/footer/footer-3.jsx
@@ -1,4 +1,4 @@
-import '../../styles/footer-styles/footer-3.css';
+
 import Square from '../../assets/square.svg'
 import Apple from '../../assets/apple1.svg'
 import Google from '../../assets/google.svg'
@@ -9,12 +9,12 @@ function Footer3 ()
 {
     return (
         <>
-            <div className="footer-3-global">
-                <div className="footer-3-1">
+            <div className="flex flex-wrap items-end justify-center py-10 space-x-10  space-y-6 md:space-y-0">
+                <div className="flex justify-center space-x-10 ">
                     <img src={Square} />
                     <img src={Apple} />
-                </div>
-                <div className="footer-3-2">
+                </div> 
+                <div className="flex  itmes-center justify-center space-x-10">   
                     <img src={Google} />
                     <img src={Amazon} />
                 </div>
