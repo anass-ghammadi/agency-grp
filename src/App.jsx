0b3b109ef4613d49f66e10a87e0e5b7c1d715373
anass-ghammadi <anassghammadi@gmail.com>
2022-01-18 23:47:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Menu from "./components/Menu/Menu";
-import MenuMobile from "./components/Menu/menu-mobile";
 import Home from "./pages/home";
 import Services from "./pages/Services";
 import About from "./pages/About";
