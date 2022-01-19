487610ea3a7fe11194c70156a37eae69ec24891c
anass-ghammadi <anassghammadi@gmail.com>
2022-01-19 13:23:10 +0100

    Update Hero-S.jsx

--- src/components/molecule/Services/Hero-S.jsx
+++ src/components/molecule/Services/Hero-S.jsx
@@ -22,25 +22,27 @@ function HeroS ()
 
     return (
         <>
-            <div className="flex flex-col min-h-max w-full h-screen mx-auto " style={{ background:"linear-gradient(251.55deg, rgba(119, 23, 194, 0.84) 13.25%, rgba(82, 22, 160, 0.891418) 45.83%, rgba(31, 21, 140, 0.92) 90.55%)"}}>
+            <div className="flex flex-col   min-h-max w-full h-screen mx-auto " style={{ background:"linear-gradient(251.55deg, rgba(119, 23, 194, 0.84) 13.25%, rgba(82, 22, 160, 0.891418) 45.83%, rgba(31, 21, 140, 0.92) 90.55%)"}}>
                 <Menu toggle={toggle} />
                 <MenuMobile isOpen={isOpen} toggle={toggle} />
                 {/*waves*/}
-                <div className="flex flex-col items-start justify-center  mx-auto  max-w-7xl h-full space-y-2 w-72 sm:w-96  md:mx-16 xl:mx-36 ">
-                    <img className="" src={Cube1}/>
-                    <h1 className="text-white text-2xl font-bold md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h1>
-                    <p className="text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet sagittis sagittis massa molestie in posuere bibendum. Diam sit aliquet congue eget suscipit tincidunt. Sapien, iaculis tempor facilisis diam aliquam. Fringilla orci, sollicitudin nec condimentum. Gravida nibh ut arcu ridiculus. Massa interdum sagittis senectus donec elementum, magna mus imperdiet.</p>
-                </div>
-                <div className=" flex flex-row items-center justify-center mx-auto max-w-7xl h-full space-x-3 py-10 ">
-                     <img className="w-24 sm:block hidden" src={Cube2}/>                   
-                    <img  className="sm:block hidden"src={Ae}/>
-                    <img className="sm:block hidden"src={Figma}/>
-                    <img  className="sm:block hidden"src={Unity}/>
-                    <img className="sm:block hidden" src={Ui}/>
-                    <img  className="w-14" src={Flutter}/>
-                    <img className="w-14" src={React}/>
-                    <img  className="w-14"src={Vue}/>
-                    <img  className="w-14" src={Js}/>   
+                <div className="flex flex-col">
+                    <div className="flex flex-col items-start justify-center  mx-auto  max-w-7xl h-full space-y-2 w-72 sm:w-96  md:mx-16 xl:mx-36 ">
+                        <img className="" src={Cube1}/>
+                        <h1 className="text-white text-2xl font-bold md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h1>
+                        <p className="text-sm text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquet sagittis sagittis massa molestie in posuere bibendum. Diam sit aliquet congue eget suscipit tincidunt. Sapien, iaculis tempor facilisis diam aliquam. Fringilla orci, sollicitudin nec condimentum. Gravida nibh ut arcu ridiculus. Massa interdum sagittis senectus donec elementum, magna mus imperdiet.</p>
+                    </div>
+                    <div className=" flex flex-row items-center justify-center mx-auto max-w-7xl h-full space-x-3 py-10 md:flex-row-reverse md:mx-16 xl:mx-36 md:items-end md:justify-start">
+                        <img className="w-24 md:w-32 sm:block hidden" src={Cube2}/>                   
+                        <img  className="sm:block hidden md:w-14"src={Ae}/>
+                        <img className="sm:block hidden md:w-14"src={Figma}/>
+                        <img  className="sm:block hidden md:w-14"src={Unity}/>
+                        <img className="sm:block hidden md:w-14" src={Ui}/>
+                        <img  className="w-14 md:w-14" src={Flutter}/>
+                        <img className="w-14 md:w-14" src={React}/>
+                        <img  className="w-14 md:w-14"src={Vue}/>
+                        <img  className="w-14 md:w-14" src={Js}/>   
+                    </div>
                 </div>
             </div>
         </>
