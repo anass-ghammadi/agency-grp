a63c5b1ec3eabd9c93a4794f53ae1456ea20342b
anass-ghammadi <anassghammadi@gmail.com>
2022-01-20 14:28:35 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
+import HeroA from "../components/molecule/About/Hero-A"
+import DefaultLayout from "../components/common/Layout/DefaultLayout"
+import SectionP from "../components/molecule/About/Section-p"
+import Footer1 from "../components/footer/footer-1"
+import Footer2 from "../components/footer/footer-2"
+import Footer3 from "../components/footer/footer-3"
+import Footer4 from "../components/footer/footer-4"
+
+
+
 const About = () => {
     return (
         <>
-           <p>salut ici c'est About</p>
+           <HeroA />
+           <DefaultLayout>
+                <SectionP />
+                <Footer1 />
+               <Footer2 />
+               <Footer3 />
+           </DefaultLayout>
+           
+           <Footer4 />
         </>
     )
 }
