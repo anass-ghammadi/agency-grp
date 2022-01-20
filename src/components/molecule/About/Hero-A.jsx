5c63de9213162bd9ac413e427d713e9a99c72838
anass-ghammadi <anassghammadi@gmail.com>
2022-01-20 14:28:48 +0100

    Create Hero-A.jsx

---
+++ src/components/molecule/About/Hero-A.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Menu from"../../Menu/Menu"
+import MenuMobile from"../../Menu/menu-mobile"
+import Cover1 from "../../../assets/Build.png";
+import Etoile from "../../../assets/etoile.svg";
+
+function HeroA ()
+{
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return(
+        <>
+            <div className=" flex flex-col items-center justify-center" style={{ background:"linear-gradient(270.28deg, rgba(14, 150, 169, 0.65) 23.98%, rgba(71, 30, 158, 0.81) 88.03%)"}}>
+                <Menu toggle={toggle} />
+                <MenuMobile isOpen={isOpen} toggle={toggle} />
+                <div className=" flex flex-col items-center justify-center mx-auto  max-w-7xl h-full space-y-8 py-10 md:flex-row  md:justify-around">
+                    <h1 className="text-2xl text-white font-semibold">A propos de l'agence</h1>
+                    <div className="flex flex-col items-center justify-between p-3 rounded-2xl max-w-[270px] lg:w-64 h-[24rem] drop-shadow-lg hover:scale-125  duration-700 " style={{background:"linear-gradient(185.55deg, rgba(141, 23, 15, 0.49) 3.08%, #640685 80.27%)"}}>
+                        <img src={Cover1}/>
+                        <h2 className="text-xl text-center font-bold text-white">Advanced Development </h2>
+                        <p className="text-white text-center">Des Architectes du web, des Réalisateurs d'idées.</p>
+                        <img src={Etoile}/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default HeroA
