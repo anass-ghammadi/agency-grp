a8e18fc28b3791e85b6debcd93e333fd4ef69c79
anass-ghammadi <anassghammadi@gmail.com>
2022-01-22 16:46:51 +0100

    Update footer-3.jsx

--- src/components/footer/footer-3.jsx
+++ src/components/footer/footer-3.jsx
@@ -2,22 +2,21 @@
 import Square from '../../assets/square.svg'
 import Apple from '../../assets/apple1.svg'
 import Google from '../../assets/google.svg'
-import Amazon from '../../assets/amazon.svg'
+import Amazon from '../../assets/amazon-2.svg'
+import Shopify from '../../assets/shopify.svg'
 
 
 function Footer3 () 
 {
     return (
         <>
-            <div className="flex flex-wrap items-end justify-center py-10 space-x-10  space-y-6 md:space-y-0">
-                <div className="flex justify-center space-x-10 ">
+            <div className="flex flex-row items-center justify-start px-4 py-10 space-x-10  section-scroll md:justify-center ">
+                
                     <img src={Square} />
-                    <img src={Apple} />
-                </div> 
-                <div className="flex  itmes-center justify-center space-x-10">   
+                    <img src={Apple} />  
                     <img src={Google} />
-                    <img src={Amazon} />
-                </div>
+                    <img className="w-28 pt-3"src={Amazon} />
+                    <img className="w-28"src={Shopify} />
             </div>
         </>
     )
