39d4caade5055de8f94b9c82b29608df721d02f9
anass-ghammadi <anassghammadi@gmail.com>
2022-01-22 16:46:49 +0100

    Update discounts.jsx

--- src/components/molecule/atoms/discounts.jsx
+++ src/components/molecule/atoms/discounts.jsx
@@ -8,7 +8,7 @@ function Discounts ()
                 <div className="z-10">
                     <img src={Reactlogo}></img>
                 </div>
-                <div className="rounded-full w-32 h-32 mt-[-20px] drop-shadow-lg" style={{ background:"linear-gradient(180deg, rgba(24, 32, 255, 0.19) 0%, rgba(24, 32, 79, 0.74) 100%)"}}> 
+                <div className="rounded-full w-32 h-32 mt-[-30px] drop-shadow-lg" style={{ background:"linear-gradient(180deg, rgba(24, 32, 255, 0.19) 0%, rgba(24, 32, 79, 0.74) 100%)"}}> 
                     <p className="text-center font-semibold text-white mt-10">50% OFF <br/>Agency</p>
                 </div>
 
