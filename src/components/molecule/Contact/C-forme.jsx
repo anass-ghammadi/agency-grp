3c473dc75032e126f74bb0d1a628a8bbc9a87974
anass-ghammadi <anassghammadi@gmail.com>
2022-01-22 16:46:45 +0100

    Update C-forme.jsx

--- src/components/molecule/Contact/C-forme.jsx
+++ src/components/molecule/Contact/C-forme.jsx
@@ -4,11 +4,16 @@ function CForme ()
 {
     return (
         <>
-            <div>
-                <div className="flex flex-col justify-center items-start p-3 w-80 py-14">
+            <div className=" py-10">
+                <div className="flex flex-col justify-center items-start p-2 w-80 py-4">
                     <p className="text-md text-gray-400">CONTACTER</p>
-                    <h1 className="text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-700 to-blue-800 " >NOTRE AGENCE</h1>
+                    <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-700 to-blue-800 " >NOTRE AGENCE</h1>
                     <p class="text-sm">Réponse sous 24h. Nous sommes également joignable par téléphone au (+212) 000000000.</p>
+                </div>
+                <div>
+                    <form>
+                        <h1 class="text-2xl text-center font-bold">Contact Form</h1>
+                    </form>
                 </div>
             </div>
         </>
