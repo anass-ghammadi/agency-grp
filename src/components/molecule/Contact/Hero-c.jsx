d5a541ab266d7b6dbfb1be9d4599462a83210ad7
anass-ghammadi <anassghammadi@gmail.com>
2022-01-26 16:22:15 +0100

    Update Hero-c.jsx

--- src/components/molecule/Contact/Hero-c.jsx
+++ src/components/molecule/Contact/Hero-c.jsx
@@ -19,7 +19,7 @@ function Heroc() {
             <div className=" flex flex-col items-center justify-center" style={{ background: "linear-gradient(270.28deg, rgba(14, 150, 169, 0.65) 23.98%, rgba(71, 30, 158, 0.81) 88.03%)" }}>
                 <Menu toggle={toggle} />
                 <MenuMobile isOpen={isOpen} toggle={toggle} />
-                <div className=" flex flex-col items-center justify-center mx-auto  max-w-7xl h-full space-y-8 py-10 md:flex-row  md:justify-around">
+                <div className=" flex flex-col justify-center mx-auto  max-w-7xl  w-full h-full py-10 md:flex-row  md:justify-around">
                     <h1 className="text-2xl text-white font-semibold">Nous Contacter</h1>
                     <CardView backgroundColor={"linear-gradient(185.55deg, rgba(141, 23, 15, 0.49) 3.08%, #640685 80.27%)"}>
                         <img src={Cover1} />
