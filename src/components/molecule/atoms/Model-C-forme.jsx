0014bbf9cd6ad09e542d5a0020a2320d54977308
anass-ghammadi <anassghammadi@gmail.com>
2022-01-27 22:07:13 +0100

    Update Model-C-forme.jsx

--- src/components/molecule/atoms/Model-C-forme.jsx
+++ src/components/molecule/atoms/Model-C-forme.jsx
@@ -5,6 +5,21 @@ function ModelForme ()
 {
     return (
         <>
+            <div>
+                <form>
+                    <label>Name</label>
+                    <input type="text" name="name"></input>
+                    <label>Tel</label>
+                    <input type="text" name="Tel"></input>
+                    <label>E Mail</label>
+                    <input type="text" name="name"></input>
+                    <label>Site web</label>
+                    <input type="text" name="name"></input>
+                    <label>Message</label>
+                    <input type="text" name="name"></input>
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </div>
         </>
     )
 }
